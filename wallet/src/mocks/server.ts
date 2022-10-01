bbebfd6c29d5bb09119d656dbb1dc2f6a778814c
HirokiYasui <hiromaily2@gmail.com>
2022-10-01 16:21:41 +0900

    try fixing esm problems

--- wallet/src/mocks/server.ts
+++ wallet/src/mocks/server.ts
@@ -1,4 +1,5 @@
 import { setupServer } from 'msw/node'
+//import { setupServer } from 'msw/node/lib/index.js'
 import { handlers } from './handlers'
 
 export const server = setupServer(...handlers)
--- wallet/src/pages/_app.tsx
+++ wallet/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 if (process.env.NODE_ENV === 'development') {
   const mockServer = () => import('@/mocks/worker')
   mockServer()
+  //require('@/mocks/worker')
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
