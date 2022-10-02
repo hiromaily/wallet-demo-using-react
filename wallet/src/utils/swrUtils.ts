e95b04bf3d7db7bc7f5241b61362d9ba1fd3c042
HirokiYasui <hiromaily2@gmail.com>
2022-10-02 18:33:27 +0900

    add swr util

---
+++ wallet/src/utils/swrUtils.ts
@@ -0,0 +1,36 @@
+import type { Middleware, SWRHook } from 'swr'
+
+// e.g.
+// useSWR(key, fetcher, { use: [endpoint] })
+//  or
+// <SWRConfig value={{ use: [endpoint] }}>
+//   <Target />
+// </SWRConfig>
+const endpoint: Middleware = (useSWRNext: SWRHook) => {
+  return (key, fetcher, config) => {
+    let newKey = key
+    if (process.env.API_ENDPOINT) newKey = `${process.env.API_ENDPOINT}${key}`
+    console.log(`newKey: ${newKey}`)
+    return useSWRNext(newKey, fetcher, config)
+  }
+}
+
+const configs = {
+  use: [endpoint],
+  // onError: (error: any, key: string) => {
+  //   // WIP how to skip Mock Service Worker specific error
+  //   if (error && typeof error === 'string' && error.includes('SyntaxError: Unexpected token'))
+  //     return
+  // },
+}
+
+const filterError: any = (error: any) => {
+  let newError: any = undefined
+  // skip Mock Service Worker specific error
+  if (error && typeof error === 'string' && error.includes('SyntaxError:')) {
+    return undefined
+  }
+  return error
+}
+
+export { endpoint, configs, filterError }
